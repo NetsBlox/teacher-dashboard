f5b0e74459561b68a4302bc732418549983ad822
Brian Broll <brian.broll@gmail.com>
2023-10-11 00:56:42 -0500

    WIP updating to latest cloud client

--- src/store/sandbox.js
+++ src/store/sandbox.js
@@ -1,5 +1,3 @@
-import axios from 'axios'
-
 function findGroup(state, groupId) {
   // there is no access to getters in mutations
   return state.groups.find(g => g._id === groupId)
@@ -143,21 +141,16 @@ export default {
   },
 
   actions: {
-    // WARN probably shouldn't use the return values from actions as they are not a reference  to the store
+    // WARN probably shouldn't use the return values from actions as they are not a reference to the store
+    // FIXME: replace these with calls using the cloud client
     async fetchGroups(context) {
-      const endpoint = context.state.SERVER_ADDRESS + '/api/groups'
-      let { data: groups } = await axios.get(endpoint, {
-        withCredentials: true,
-      })
+      const groups = await client.listGroups();  // FIXME: define the client
       context.commit('setGroups', groups);
       return groups;
     },
 
     async fetchGroup(context, groupId) {
-      const endpoint = context.state.SERVER_ADDRESS + '/api/groups/' + groupId
-      let { data: group } = await axios.get(endpoint, {
-        withCredentials: true,
-      })
+      const group = await client.viewGroup(groupId);  // FIXME: define the client
       let existingGroup = findGroup(context.state, groupId);
       if (existingGroup) {
         context.commit('updateGroup', group);
@@ -169,30 +162,18 @@ export default {
 
     async fetchGroupMembers(context, groupId) { // gets group details
       if (!groupId || groupId.length !== 24) throw new Error('invalid group id', groupId);
-      console.log('fetching users', groupId);
-      const endpoint = context.state.SERVER_ADDRESS + `/api/groups/${groupId}/members`
-      let { data: users } = await axios.get(endpoint, {
-        withCredentials: true,
-      })
-
+      const users = await client.listMembers(groupId);  // FIXME: define the client
       context.commit('setGroupData', {groupId, users });
-    },
-
-    async fetchGroupAPIKeys(context, groupId) {
-      const apiKeysUrl = `${context.state.SERVER_ADDRESS}/services/keys`
-      const { data: apiKeys } = await axios.get(apiKeysUrl, {
-        withCredentials: true,
-      })
-      context.commit('setGroupAPIKeys', {groupId, apiKeys});
     },
 
     async createUser(context, user) {
       console.debug(`creating user ${user.username}`);
       if (!user || !user.username || !user.groupId || !user.email || !user.password) throw new Error(`missing user data, ${user.username}`);
-      const endpoint = context.state.SERVER_ADDRESS + `/api/groups/${user.groupId}/members`
-      let response = await axios.post(endpoint, user, {
-        withCredentials: true
-      })
+
+      const {username, groupId, email, password} = user;
+      await client.createUser(username, email, password, groupId);
+
+      // FIXME; set createdUser
       let createdUser = response.data;
       console.debug(`created user ${createdUser}`);
       console.assert(createdUser._id !== undefined, 'malformed user response');
@@ -206,6 +187,8 @@ export default {
       console.log(`updating user ${user.username}`);
       if (!user || !user.username || !user.email) throw new Error(`missing user data, ${user}}`);
       const endpoint = context.state.SERVER_ADDRESS + `/api/groups/${user.groupId}/members/${user._id}`;
+      // TODO: update user...
+      // TODO: what types of changes can we make???
       let response = await axios.patch(endpoint, user, {
         withCredentials: true
       })
@@ -216,19 +199,14 @@ export default {
     async deleteUser(context, user) {
       console.log(`deleting user ${user.username}`);
       if (!user || !user.username || !user.email) throw new Error(`missing user data, ${user}}`);
-      const endpoint = context.state.SERVER_ADDRESS + `/api/groups/${user.groupId}/members/${user._id}`;
-      let response = await axios.delete(endpoint, {
-        withCredentials: true
-      })
+      await client.deleteUser(user.username);
       context.commit('deleteGroupUser', user);
       return response.data;
     },
 
     async setServicesHosts(context, data) {
       const {groupId, servicesHosts} = data;
-      const endpoint = `${context.state.SERVER_ADDRESS}/api/v2/services-hosts/group/${groupId}`;
-      const options = {withCredentials: true};
-      await axios.post(endpoint, servicesHosts, options);
+      await client.setGroupHosts(groupId, servicesHosts);
       context.commit('updateServicesHosts', data);
     },
 
@@ -246,22 +224,27 @@ export default {
       if (index > -1) {
         const servicesHosts = group.servicesHosts.slice();
         servicesHosts.splice(index, 1);
-        const endpoint = `${context.state.SERVER_ADDRESS}/api/v2/services-hosts/group/${groupId}`;
-        const options = {withCredentials: true};
-        await axios.post(endpoint, servicesHosts, options);
+        await client.setGroupHosts(groupId, servicesHosts);
         context.commit('updateServicesHosts', {groupId, servicesHosts});
       }
     },
 
     async createAPIKey(context, apiKey) {
       const {provider, value, groups} = apiKey;
-      const endpoint = `${context.state.SERVER_ADDRESS}/services/keys/${provider}`;
-      const options = {withCredentials: true};
+
+      // TODO: use the client instead... Set the settings for the group
       const response = await axios.post(endpoint, {value, groups}, options);
       const keyId = response.data;
       apiKey._id = keyId;
+
       context.commit('addGroupAPIKey', apiKey);
       return apiKey;
+    },
+
+    async fetchGroupAPIKeys(context, groupId) {
+      const servicesHost = context.state.SERVICES_HOST;
+      const {apiKeys} = await client.getGroupSettings(groupId, servicesHost);
+      context.commit('setGroupAPIKeys', {groupId, apiKeys});
     },
 
     async updateAPIKey(context, apiKey) {
@@ -297,21 +280,15 @@ export default {
 
     async deleteGroup(context, group) {
       console.log('deleting group', group.name);
-      const endpoint = context.state.SERVER_ADDRESS + '/api/groups/' + group._id;
-      let { data: resp } = await axios.delete(endpoint, {
-        withCredentials: true
-      })
+      await client.deleteGroup(group.id);  // FIXME: or _id?
       context.commit('deleteGroup', group);
       return resp;
     },
 
     async createGroup(context, name) {
       console.log('creating group', name);
-      const endpoint = context.state.SERVER_ADDRESS + '/api/groups'
-      let { data: group } = await axios.post(endpoint, {name: name}, {
-        withCredentials: true
-      })
-      console.assert(group._id !== undefined, 'malformed group response');
+      const group = await client.createGroup(client.username, name);
+      console.assert(group.id !== undefined, 'malformed group response');
       context.commit('addGroup', group);
       return group;
     },
