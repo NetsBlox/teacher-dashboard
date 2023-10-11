8d19855dc8ee6cdb86eef63ec734672a80627cb4
Brian Broll <brian.broll@gmail.com>
2023-10-11 01:01:16 -0500

    Add maintenance disclaimer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,8 @@
   <div class="home">
     <a :href="SERVER_ADDRESS" target="_blank"><img alt="NetsBlox logo" src="/images/logo.png"></a>
     <p>Welcome to NetsBlox teacher dashboard.</p>
+    <p style="color:red;font-weight:bold">Undergoing maintenance. Please be patient as we work on improving the system!</p>
+    <p style="font-weight:bold">For urgent requests, please reach out to brian.broll@vanderbilt.edu</p>
     <p>Server address: <a :href="SERVER_ADDRESS" target="_blank">{{ SERVER_ADDRESS }}</a></p>
   </div>
 </template>
